f0a35836376bb4f69d58ec8ecf91b9899dfe00d8
Edoardo Panichi <edoardo.panichi99@gmail.com>
2022-12-05 17:19:47 +0100

    Finished Part I, started Part II. Rotation function still in progress.

--- assignments/week_2/points_and_triangles.cpp
+++ assignments/week_2/points_and_triangles.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 // Include useful maths operation
 #include <cmath>
-#include <numbers>
 
 class Point
 {
@@ -13,34 +12,34 @@ class Point
             y(0)
         {}
 
-        Point(double x, double y)
+        Point(const double &x, const double &y)
         :   x(x),
             y(y)
         {}
 
         // OPERATORS
-        Point operator+(Point other) 
+        Point operator+(const Point &other) const
         { 
             // create a variable Point to store the result
             Point result (x + other.x, y + other.y);
             return result;
         }
 
-        Point operator-(Point other) 
+        Point operator-(const Point &other) const 
         { 
             // create a variable Point to store the result
             Point result (x - other.x, y - other.y);
             return result;
         }
 
-        Point &operator+=(Point other) 
+        Point &operator+=(const Point &other)
         {
             x = x + other.x;
             y = y + other.y;
             return *this;
         }
 
-        Point &operator-=(Point other) 
+        Point &operator-=(const Point &other)
         { 
             x = x - other.x;
             y = y - other.y;
@@ -53,21 +52,21 @@ class Point
 
         // METHODS
         // this function returns the distance from the origin
-        double distance() 
+        double distance() const
         {
             double distance_origin;
             distance_origin = sqrt(pow(x, 2) + pow(y, 2));
             return distance_origin;
         }
 
-        double distance(Point other) 
+        double distance(const Point &other) const 
         {
             double distance_points;
             distance_points = sqrt(pow((x - other.x), 2) + pow((y - other.y), 2));
             return distance_points;
         }
 
-        Point rotated(double angle)
+        Point rotated(double angle) const
         {
             // Initialization of the output
             Point point_rotated;
@@ -82,7 +81,7 @@ class Point
             return point_rotated;
         }
 
-        Point rotated(double angle, Point other)
+        Point rotated(double angle, const Point &other) const
         {
             // Initialization of the output
             Point point_rotated;
@@ -90,23 +89,24 @@ class Point
             double s = sin(angle);
             double c = cos(angle);
 
+            // Creating temporary variable that are not constant and can be modified
+            double x_temp = x;
+            double y_temp = y;
             // Translating the point back to the origin
-            x -= other.x;
-            y -= other.y;
+            x_temp -= other.x;
+            y_temp -= other.y;
 
             // rotate point
-            point_rotated.x = x * c - y * s;
-            point_rotated.y = x * s + y * c;
+            point_rotated.x = x_temp * c - y_temp * s;
+            point_rotated.y = x_temp * s + y_temp * c;
 
-            // Applying the translation back again (this time also on point_rotated)
+            // Applying the translation back again on point_rotated
             point_rotated += other;
-            x += other.x;
-            y += other.y;
 
             return point_rotated;
         }
 
-        Point& rotate(double angle) 
+        Point& rotate(double angle)
         { 
             // Simplification of the notation
             double s = sin(angle);
@@ -121,7 +121,7 @@ class Point
             return *this; 
         }
 
-        Point& rotate(double angle, const Point other) 
+        Point& rotate(double angle, const Point &other) 
         {
             // Simplification of the notation
             double s = sin(angle);
@@ -147,9 +147,64 @@ class Point
 
 };
 
+class Triangle
+{
+    public:
+        // CONSTRUCTORS
+        Triangle()
+        :   a(),
+            b(),
+            c()
+        {}
+
+        Triangle(const Point &A, const Point &B, const Point &C)
+        :   a(A.x, A.y),
+            b(B.x, B.y),
+            c(C.x, C.y)
+        {}
+
+        // OPERATORS    
+        // ATTRIBUTES
+        Point a, b, c;
+
+        // METHODS
+        Triangle translated(const Point &t) const 
+        {
+            // Create the output variable
+            Triangle translated_triang(a, b, c);
+
+            // Applying the translation
+            translated_triang.a += t;
+            translated_triang.b += t;
+            translated_triang.c += t;
+
+            return translated_triang;
+        }
+
+        Triangle& translate(const Point &t)
+        {
+            // Applying the translation
+            a += t;
+            b += t;
+            c += t;
+
+            return *this;
+        }
+
+        Triangle rotated(double angle) const
+        {
+            // creating the new triangle to return
+            Triangle rotated_triang = *this;
+            
+        }
+        // Triangle rotated(double angle, Point other) { ... }
+
+};
+
 int main(){
+    // ASSIGNMENT PART I
     Point A(2, 4);
-    Point B = {3, 5};
+    Point B(3, 5);
     Point C(4, 5);
 
     B += A;
@@ -161,14 +216,13 @@ int main(){
     Point D = A.rotated(pi/2);
     Point E = A.rotated(pi/2, B);
 
-    Point F = {0, 1};
-    Point G = {2, 4};
+    Point F(0, 1);
+    Point G(2, 4);
     F.rotate(pi / 2);
     G.rotate(pi/2, B);
 
-
     // Testing Part I of the assignment
-    std::cout << "A: " << A.x << " " << A.y << std::endl;
+    std::cout << "PART I\n" << "A: " << A.x << " " << A.y << std::endl;
     std::cout << "B: " << B.x << " " << B.y << std::endl;
     std::cout << "C: " << C.x << " " << C.y << std::endl;
     std::cout << "distance of A from the origin: " << dist_orig << std::endl;
@@ -177,5 +231,17 @@ int main(){
     std::cout << "Rotation of A around B: " << E.x << " " << E.y << std::endl;
     std::cout << "Rotation of F around the origin: " << F.x << " " << F.y << std::endl;
     std::cout << "Rotation of G around B: " << E.x << " " << E.y << std::endl;
+
+    // ASSIGNMENT PART II
+    Triangle tr_test;
+    Triangle ABC(A, B, C);
+
+    Triangle translated = ABC.translated(A);
+    tr_test.translate(C);
+
+    // Testing Part II of the assignment
+    std::cout << "\nPART II\n" << "Triangle ABC is: " << "Vertex 1: " << ABC.a.x << " " << ABC.a.y << " Vertex 2: " << ABC.b.x << " " << ABC.b.y << " Vertex 3: " << ABC.c.x << " " << ABC.c.y << std::endl;
+    std::cout << "Triangle ABC translated by A is: " << "Vertex 1: " << translated.a.x << " " << translated.a.y << " Vertex 2: " << translated.b.x << " " << translated.b.y << " Vertex 3: " << translated.c.x << " " << translated.c.y << std::endl;
+    std::cout << "Triangle test translated by C is: " << "Vertex 1: " << tr_test.a.x << " " << tr_test.a.y << " Vertex 2: " << tr_test.b.x << " " << tr_test.b.y << " Vertex 3: " << tr_test.c.x << " " << tr_test.c.y << std::endl;
 
 }
